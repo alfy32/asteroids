21d80bfaab7e79a627dfc1cd22d2898f9cb68f6a
nethkade <K.A.N@aggiemail.usu.edu>
2014-04-02 13:40:51 -0600

    ajax highscores

--- client/js/highscores.js
+++ client/js/highscores.js
@@ -17,17 +17,19 @@ ASTEROIDGAME.screens['high-scores'] = (function() {
       success: function(result){
                   ASTEROIDGAME.highScores = JSON.parse(result);
                   console.log(ASTEROIDGAME.highScores);
+
+                  $('.showScores').empty();
+
+                  //show top 10 high scores
+                  var totalScores = ASTEROIDGAME.highScores.length < 10 ? ASTEROIDGAME.highScores.length : 10;
+                  for(var i=0; i<totalScores; ++i){
+                    var S = ASTEROIDGAME.highScores[i];
+                    $('.showScores').append('<h2>' + S.name + ": " + S.score + "</h2>");
+                  }
               }
     });
 
-    $('.showScores').empty();
-
-    //show top 10 high scores
-    var totalScores = ASTEROIDGAME.highScores.length < 10 ? ASTEROIDGAME.highScores.length : 10;
-    for(var i=0; i<totalScores; ++i){
-      var S = ASTEROIDGAME.highScores[i];
-      $('.showScores').append('<h2>' + S.name + ": " + S.score + "</h2>");
-    }
+    
   }
 
   return {
